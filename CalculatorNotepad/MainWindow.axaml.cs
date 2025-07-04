90686d5f6964bb3eb1fc8b5ec8c5a13356d2be4e
DavidHawkes <db300@163.com>
2025-07-04 10:53:14 +0800

    增加一行转多行功能

--- CalculatorNotepad/MainWindow.axaml.cs
+++ CalculatorNotepad/MainWindow.axaml.cs
@@ -1,12 +1,30 @@
 using Avalonia.Controls;
+using System;
 
 namespace CalculatorNotepad
 {
     public partial class MainWindow : Window
     {
+        #region constructor
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region event handler
+        private void MenuItemSplitLine_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var textBox = _txtNoteBook;
+            var text = textBox.Text;
+
+            if (string.IsNullOrWhiteSpace(text)) return;
+
+            // 按空格或逗号分割
+            var lines = text.Split([' ', '，', ','], StringSplitOptions.RemoveEmptyEntries);
+            var newText = string.Join(Environment.NewLine, lines);
+            textBox.Text = newText;
+        }
+        #endregion
     }
 }
